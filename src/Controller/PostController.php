a0a9fc8ed032e7686eff1c011ceba04eb0af9403
Kilian MINETTO <killian.minetto@gmail.com>
2021-10-02 15:35:33 +0200

    Installation de serializer + PostController

---
+++ src/Controller/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * Class PostController
+ * @package App\Controller
+ * @Route("/posts")
+ */
+class PostController
+{
+    /**
+     * @Route(name="api_posts_collection_get", methods={"GET"})
+     * @param PostRepository $postRepository
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    public function collection(PostRepository $postRepository, SerializerInterface $serializer): JsonResponse
+    {
+        return new JsonResponse(
+            $serializer->serialize($postRepository->findAll(), "json"),
+            JsonResponse::HTTP_OK,
+            [],
+            true
+        );
+    }
+}
